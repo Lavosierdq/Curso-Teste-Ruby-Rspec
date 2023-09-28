2f1af74a6e4f1d45e35fca5300c6e340416dd95c
Lavosier Barbosa <lavosierbarbosa@gmail.com>
2023-09-27 23:31:44 -0300

    arquivo da aula 5 de teste RSPEC.

---
+++ spec/lib/aula5/aula5.rb
@@ -0,0 +1,20 @@
+#inicinaodo projeto ruby in rails
+## rails new nome_do projeto
+
+#configurando rspec dentro de uma gem File
+=begin
+   group:development,:test do
+     gem 'rspec-rails','~>3.8'
+   end
+=end
+# executar o comando bundle install  para o bundle adicionar a gem do rspec
+## bundle install
+
+# criar um banco de dados para o rails
+## rails db:create
+
+#instale o rspec
+## rails generate rspec:install
+
+# rodando testes
+## bundle exec rspec
