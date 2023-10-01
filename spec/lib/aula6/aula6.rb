6ad1dde5a6c93fb72395806bdeced884de503cc1
Lavosier Barbosa <lavosierbarbosa@gmail.com>
2023-09-30 22:40:02 -0300

    Aula sobre Generatos e criação de scripts

---
+++ spec/lib/aula6/aula6.rb
@@ -0,0 +1,12 @@
+## Utiliizando generatos
+# generatos  serve para gerar scripts e arquivos de testes
+# com estruturas prontas para criar testes de forma mais rápida e eficiente
+
+## criar um generator com teste para model
+#rails generate model user
+
+##Gerar Somente Teste
+#rails generate rspec:request user
+
+##Comando para verificar todos os generates para o rspec
+#rails generate --help|grep rspec
