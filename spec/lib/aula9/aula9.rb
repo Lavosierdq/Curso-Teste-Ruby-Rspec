ca78880e640c66a85b5609f85c01aa01f32bca51
Lavosier Barbosa <lavosierbarbosa@gmail.com>
2023-10-10 23:21:17 -0300

    mudanças  nos arquivos da aula 14

--- spec/lib/aula9/aula9.rb
+++ spec/lib/aula9/aula9.rb
@@ -5,7 +5,8 @@
 ## criar um generator com teste para model
 #rails generate model user
 
-##Gerar Somente Teste
+##Gerar Somente Teste.. teste de requeste usado para simular uma
+##API e ver os resultados.
 #rails generate rspec:request user
 
 ##Comando para verificar todos os generates para o rspec
