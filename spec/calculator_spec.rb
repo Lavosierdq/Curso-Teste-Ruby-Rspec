618f95390477da587bcf1a85d0afd6a0121f767d
Lavosier Barbosa <lavosierbarbosa@gmail.com>
2023-11-11 23:19:32 -0300

    arquivo das aulas de Rspec e sobre comando context

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,15 +2,17 @@
 require_relative 'lib/aula7/calculator'
 
 describe Calculator do
-  it "Testar metodo sum para dois numeros" do
-  calc  =  Calculator.new
-  result = calc.sum(10,20)
-  expect(result).to eq(30)
-  end
+    context 'sum' do #-> referencia a metodo de instância 
+      it "Testar metodo para numeros positivos" do
+      calc  =  Calculator.new
+      result = calc.sum(10,20)
+      expect(result).to eq(30)
+      end
 
-  it "Testar metodo sum para dois numeros negativos" do
-    calc  =  Calculator.new
-    result = calc.sum(-10,20)
-    expect(result).to eq(10)
-  end
+      it "Testar metodo para numeros negativos" do
+        calc  =  Calculator.new
+        result = calc.sum(-10,20)
+        expect(result).to eq(10)
+      end
+    end
 end
