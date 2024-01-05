b71b5e02c5325dcafe23042eca6c32954a5c53c0
Lavosier Barbosa <lavosierbarbosa@gmail.com>
2024-01-04 23:28:30 -0300

    exercicios da aula 17 curso Rspec com rails

---
+++ spec/lib/aula17/exercicio/exercicio1.rb
@@ -0,0 +1,24 @@
+#matchers de comparacao
+
+describe " matchers  verdadeiro ou falso" do
+
+  it 'be true' do
+    expect(2.even?).to be true
+  end
+
+  it 'be false' do
+    expect(2.odd?).to be false
+  end
+
+  it 'be nil' do
+    expect(defined? x).to be_nil
+  end
+
+  it 'be_truthy' do
+    expect(2.even?).to be_truthy 
+  end
+
+  it 'be_falsey' do
+    expect(2.odd?).to be_falsey
+  end
+end 
