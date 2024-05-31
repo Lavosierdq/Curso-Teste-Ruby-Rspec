20ea0642934498fae16158296cc4a733cc5ade19
Lavosier <lavosierbarbosa@gmail.com>
2024-05-30 23:10:36 -0300

    fazendo revisão aula 16

---
+++ lib/aula16/exercicios/exercicio2_spec.rb
@@ -0,0 +1,37 @@
+require_relative '../../../spec/spec_helper'
+
+describe "matcher de comparacao" do
+  context "texta objetos:"  do
+    it "#equal" do
+      y = 'ruby'
+      x = 'ruby'
+      expect(x).to equal(x)
+      expect(x).to_not equal(y)
+    end
+
+    it "#be" do
+      y = "ruby"
+      x = "ruby"
+      expect(x).to equal(x)
+      expect(x).to_not equal(y)
+    end
+  end
+
+  context " testa valor dos objetos" do
+    it "#eql" do
+      y = ruby
+      x = ruby
+      expect(x).to eql(y)
+      expect(x).to eql(x)
+    end
+  end
+
+  context "testa o valor dos objetos" do
+    it "eq" do
+      y = "ruby"
+      x = "ruby"
+      expect(x).to eq(y)
+      expect(x).to eq(x)
+    end
+  end
+end
