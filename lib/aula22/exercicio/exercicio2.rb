a57f682597e6c1d1d8030a5169b42fade23ebdd1
Lavosier <lavosierbarbosa@gmail.com>
2024-07-16 00:11:38 -0300

    feat: aula 22 e 23 revisão

--- lib/aula22/exercicio/exercicio2.rb
+++ lib/aula22/exercicio/exercicio2.rb
@@ -6,7 +6,23 @@ describe Calculator do
     expect(subject.divisao(3,-1)).to eq(-3)
   end
 
-  it 'dividindo por zero' do
-    expect{subject.divisao(3.0)}.to raise_exception
+  it 'usando raise_exception' do
+    expect{subject.divisao(3,0)}.to raise_exception 
+  end
+
+  it 'usando raise_error' do
+    expect{subject.divisao(3,0)}.to raise_error(ZeroDivisionError)
+  end
+
+  it 'usando raise_error com texto' do
+    expect{subject.divisao(3,0)}.to raise_error("divided by 0")
+  end
+
+  it 'usando raise_error com erro e texto' do
+    expect{subject.divisao(3,0)}.to raise_error(ZeroDivisionError,"divided by 0")
+  end
+
+  it 'usando raise error com expressao regular' do
+    expect{subject.divisao(3,0)}.to raise_error(/divi/)
   end
 end
